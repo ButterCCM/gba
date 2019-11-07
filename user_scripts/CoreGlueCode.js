1101a1cb0474b29bdc84e856c1d646ecc78169b9
ButterCCM <christophermalanga1@gmail.com>
2019-11-07 07:58:05 -0500

    Update CoreGlueCode.js

--- user_scripts/CoreGlueCode.js
+++ user_scripts/CoreGlueCode.js
@@ -63,6 +63,7 @@ var games = {
     "megaman_zero4":"Megaman Zero 4",
     "metalslug":"Metal Slug Advance",
     "metroid_fusion":"Metroid Fusion",
+    "metroid_zmission":"Metroid Zero Mission",
     "momotarou_dentetsu":"Momotarou Dentetsu G Gold Deck wo Tsukure!",
     "monopoly":"Monopoly",
     "monster_force":"Monster Force",
